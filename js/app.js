247533638f0c8709f81f5f8b7fbbfc978ed302c9
manyblake <manyblake@gmail.com>
2022-07-21 15:50:27 +0200

    start css

--- js/app.js
+++ js/app.js
@@ -2,20 +2,30 @@ const username = document.querySelector(`#username`),
   km = document.querySelector(`#km`),
   age = document.querySelector(`#age`),
   button = document.querySelector(`#generate-ticket`),
-  ticketPrice = document.querySelector(`.price`);
+  ticketPrice = document.querySelector(`.price`),
+  travelerName = document.querySelector(`.traveler`);
 
-const totalPrice = km.value * 0.21;
-const discountUnder18 = totalPrice * 0.2;
-const discountOver65 = totalPrice * 0.4;
+const PRICE_FOR_KM = 0.21,
+  DISCOUNT_UNDER_18 = 0.2,
+  DISCOUNT_OVER_65 = 0.4;
+
+const totalPrice = km.value * PRICE_FOR_KM;
+const discountUnder18 = totalPrice * DISCOUNT_UNDER_18;
+const discountOver65 = totalPrice * DISCOUNT_OVER_65;
 
 button.addEventListener("click", function () {
-  if (age.value < 18) {
-    const discountedTicket = totalPrice - discountUnder18;
-    ticketPrice.innerHTML = `${Math.round(discountedTicket * 100) / 100} €`;
-  } else if (age.value > 65) {
-    const discountedTicket = totalPrice - discountOver65;
-    ticketPrice.innerHTML = `${Math.round(discountedTicket * 100) / 100} €`;
+  if (!isNaN(km.value)) {
+    if (age.value === `minorenne`) {
+      const discountedTicket = totalPrice - discountUnder18;
+      ticketPrice.innerHTML = `${Math.round(discountedTicket * 100) / 100} €`;
+    } else if (age.value === `over65`) {
+      const discountedTicket = totalPrice - discountOver65;
+      ticketPrice.innerHTML = `${Math.round(discountedTicket * 100) / 100} €`;
+    } else {
+      ticketPrice.innerHTML = `${Math.round(totalPrice * 100) / 100} €`;
+    }
   } else {
-    ticketPrice.innerHTML = `${Math.round(totalPrice * 100) / 100} €`;
+    alert(`I dati inseriti devono essere numeri`);
   }
+  travelerName.innerHTML = username.value;
 });
