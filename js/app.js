7c29d87ad211ea5787c8302dda10be84f6748bee
manyblake <manyblake@gmail.com>
2022-07-21 17:33:51 +0200

    add cancelbutton eventlistener

--- js/app.js
+++ js/app.js
@@ -3,16 +3,20 @@ const username = document.querySelector(`#username`),
   age = document.querySelector(`#age`),
   button = document.querySelector(`#generate-ticket`),
   ticketPrice = document.querySelector(`.price`),
-  travelerName = document.querySelector(`.traveler`);
+  travelerName = document.querySelector(`.traveler`),
+  ticketType = document.querySelector(`.ticket-type`),
+  carriage = document.querySelector(`.carriage`),
+  cpCode = document.querySelector(`.cp-code`),
+  cancelButton = document.querySelector(`#cancel`);
 
 const PRICE_FOR_KM = 0.21,
   DISCOUNT_UNDER_18 = 0.2,
   DISCOUNT_OVER_65 = 0.4;
 
 const totalPrice = km.value * PRICE_FOR_KM;
-let discount = 0;
 
 button.addEventListener("click", function () {
+  let discount = 0;
   if (!isNaN(km.value)) {
     if (age.value === `minorenne`) {
       discount = totalPrice * DISCOUNT_UNDER_18;
@@ -20,12 +24,24 @@ button.addEventListener("click", function () {
       discount = totalPrice * DISCOUNT_OVER_65;
     }
 
+    travelerName.innerHTML = username.value;
+    travelerName.classList.add("username-style");
+
+    ticketType.innerHTML = `Biglietto Standard`;
+
+    carriage.innerHTML = `5`;
+
+    cpCode.innerHTML = `92911`;
+
     ticketPrice.innerHTML = `${
       Math.round((totalPrice - discount) * 100) / 100
     } €`;
   } else {
     alert(`I dati inseriti devono essere numeri`);
   }
+});
 
-  travelerName.innerHTML = username.value;
+cancelButton.addEventListener("click", function () {
+  username.value = null;
+  km.value = null;
 });
