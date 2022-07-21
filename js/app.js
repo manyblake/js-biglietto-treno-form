17e14954b3375f4c5e4a0de5d771291a59f0d14e
manyblake <manyblake@gmail.com>
2022-07-21 16:11:37 +0200

    refactor js logic

--- js/app.js
+++ js/app.js
@@ -10,22 +10,22 @@ const PRICE_FOR_KM = 0.21,
   DISCOUNT_OVER_65 = 0.4;
 
 const totalPrice = km.value * PRICE_FOR_KM;
-const discountUnder18 = totalPrice * DISCOUNT_UNDER_18;
-const discountOver65 = totalPrice * DISCOUNT_OVER_65;
+let discount = 0;
 
 button.addEventListener("click", function () {
   if (!isNaN(km.value)) {
     if (age.value === `minorenne`) {
-      const discountedTicket = totalPrice - discountUnder18;
-      ticketPrice.innerHTML = `${Math.round(discountedTicket * 100) / 100} €`;
+      discount = totalPrice * DISCOUNT_UNDER_18;
     } else if (age.value === `over65`) {
-      const discountedTicket = totalPrice - discountOver65;
-      ticketPrice.innerHTML = `${Math.round(discountedTicket * 100) / 100} €`;
-    } else {
-      ticketPrice.innerHTML = `${Math.round(totalPrice * 100) / 100} €`;
+      discount = totalPrice * DISCOUNT_OVER_65;
     }
+
+    ticketPrice.innerHTML = `${
+      Math.round((totalPrice - discount) * 100) / 100
+    } €`;
   } else {
     alert(`I dati inseriti devono essere numeri`);
   }
+
   travelerName.innerHTML = username.value;
 });
